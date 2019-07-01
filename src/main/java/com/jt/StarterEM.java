971b841cae7d76b559c054e8bcabec4c20df1134
eclipce_user <eclipse_user@zhouwei.com>
2019-07-01 17:03:57 +0800

    分支实战，我的分支hot_fix

--- src/main/java/com/jt/StarterEM.java
+++ src/main/java/com/jt/StarterEM.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.jt.easymall.mapper")
 public class StarterEM {
 	public static void main(String[] args) {
+		//分支实战，我的分支hot_fix
 		SpringApplication.run(StarterEM.class, args);
 	}
 }
